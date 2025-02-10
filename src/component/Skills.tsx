91c4674673ae29e9148652ec8121e02a514ebc2a
nitish619 <nitishchavan937@gmail.com>
2025-02-10 16:07:28 +0530

    modify in components

--- src/component/Skills.tsx
+++ src/component/Skills.tsx
@@ -12,7 +12,7 @@ function Skills() {
     { name: "TypeScript", icon: <SiTypescript className="text-blue-600 text-4xl" /> },
     { name: "Python", icon: <FaPython className="text-yellow-500 text-4xl" /> },
     { name: "MERN Stack", icon: <SiMongodb className="text-green-500 text-4xl" /> },
-    { name: "Express.js", icon: <SiExpress className="text-gray-700 text-4xl" /> },
+    { name: "Express.js", icon: <SiExpress className="text-yellow-300 text-4xl" /> },
     { name: "Node.js", icon: <FaNodeJs className="text-green-500 text-4xl" /> },
     { name: "Git", icon: <FaGit className="text-orange-500 text-4xl" /> },
     { name: "GitHub", icon: <FaGithub className="text-gray-400 text-4xl" /> },
@@ -25,7 +25,7 @@ function Skills() {
           <h2 className="text-4xl font-bold text-blue-400 text-center mb-10">Skills</h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 p-6">
             {skills.map((skill, index) => (
-              <div key={index} className="flex flex-col items-center p-4 bg-slate-800 rounded-xl shadow-md transition-transform hover:scale-105">
+              <div key={index} className="flex flex-col items-center p-4 bg-slate-800 rounded-xl shadow-md transition-transform hover:scale-105 hover:bg-slate-700 hover:shadow-lg hover:shadow-slate-400">
                 {skill.icon}
                 <p className="mt-2 text-white font-medium">{skill.name}</p>
               </div>
