56b675132abbf73ace8901461d0aa1e3d372982d
nitish619 <nitishchavan937@gmail.com>
2025-02-10 16:02:51 +0530

    modify in components

---
+++ src/assets/index.js
@@ -0,0 +1,66 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import nextjs from "./tech/nextjs.png";
+import mysql from "./tech/mysql.png";
+import python from "./tech/python.png";
+import github1 from "./tech/github1.png";
+
+import Jagruti from "./company/jagruti.png";
+
+import jobit from "./jobit.png";
+import objectDetect from "./object-detect.png";
+import stm32 from "./stm32.png";
+import yt from "./yt.png";
+import nikestore from "./nikestore.png";
+import frostEcommerce from "./frostecommerce.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  nextjs,
+  tailwind,
+  typescript,
+  jobit,
+  mysql,
+  python,
+  github1,
+  Jagruti,
+  objectDetect,
+  stm32,
+  yt,
+  nikestore,
+  frostEcommerce,
+};
